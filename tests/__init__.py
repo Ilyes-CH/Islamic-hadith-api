801444516995be0c06a270bf555b15f4c341333d
ilyes <120855231+Ilyes-CH@users.noreply.github.com>
2023-05-14 22:10:24 +0100

    Delete tests directory

--- tests/__init__.py
+++
@@ -1,6 +0,0 @@
-
-from flask import *
-from tests.routes import *
-
-if __name__=='__main__':
-    app.run(port=9999, debug=True)
--- tests/switch.py
+++
@@ -1,9 +0,0 @@
-weather : str = "good"
-
-match(weather):
-    case("good"):
-        print('good weather')
-    case('not good'):
-        print('bad weather')
-    case _:
-        pass
