10937e5e6346f6a96bed469938255d99a9b060f6
ilyes <120855231+Ilyes-CH@users.noreply.github.com>
2023-05-15 01:40:10 +0100

    Update api.py

--- api.py
+++ api.py
@@ -1,33 +1,39 @@
-from flask import Flask, render_template
+from flask import Flask, jsonify, render_template
 import json
 
-app = Flask(__name__,template_folder='template')
+app = Flask(__name__, template_folder='template')
 
-
-@app.route('/',methods=["GET"])
+@app.route('/', methods=["GET"])
 def home():
     return render_template('index.html')
 
-
-@app.route('/api/data')
+@app.route('/api/data', methods=["GET"])
 def api_data():
-   try:
-        with open('hadith.json','r') as f:
-            hadiths=json.load(f)
-            
-        data = {'Headers':{
-            'message':'hello from server',
-            'status': '200 ok',
-            },
-                'content': hadiths
-                }
+    try:
+        with open('hadith.json', 'r') as f:
+            hadiths = json.load(f)
 
-        return data , 200
-    
-   except:
-       print('Error 500')
-       api_data()
+        response = {
+            'message': 'Data retrieved successfully',
+            'status': 200,
+            'data': hadiths
+        }
 
+        return jsonify(response), 200
 
-if __name__=='__main__':
+    except FileNotFoundError:
+        response = {
+            'message': 'Resource not found',
+            'status': 404
+        }
+        return jsonify(response), 404
+
+    except Exception as e:
+        response = {
+            'message': 'Internal server error',
+            'status': 500.
+        }
+        return jsonify(response), 500
+
+if __name__ == '__main__':
     app.run(port=9999, debug=True)
